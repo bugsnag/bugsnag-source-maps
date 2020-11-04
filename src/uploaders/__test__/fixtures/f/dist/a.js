08237a57fcf19ded018020d6be30e7d6f05b3722
Ben Gourley <bn@grly.me>
2020-11-04 15:21:54 +0000

    test: Ensure gitignore'd tests are included

---
+++ src/uploaders/__test__/fixtures/f/dist/a.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function a() {
+    console.log('hi from a');
+}
+exports.default = a;
+//# sourceMappingURL=a.js.map
---
+++ src/uploaders/__test__/fixtures/f/dist/b.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function a() {
+    console.log('hi from b');
+}
+exports.default = a;
+//# sourceMappingURL=b.js.map
