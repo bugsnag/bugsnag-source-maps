c40c5a58db641825ef5d59442f96f71c3020ca68
Steve Kirkland <steve@bugsnag.com>
2021-04-27 22:04:44 +0100

    Convert to Docker

--- scripts/license_finder.sh
+++ scripts/license_finder.sh
@@ -4,6 +4,5 @@
 curl https://raw.githubusercontent.com/bugsnag/license-audit/master/config/decision_files/global.yml -o config/decisions.yml
 curl https://raw.githubusercontent.com/bugsnag/license-audit/master/config/decision_files/common-js.yml >> config/decisions.yml
 
-gem install license_finder
 npm install
 license_finder -d --decisions-file=config/decisions.yml --enabled-package-managers=npm
