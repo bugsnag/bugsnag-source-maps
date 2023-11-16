17910c3883db599085eca0b871b4ef6b407ae727
Tom Longridge <tom.longridge@smartbear.com>
2023-11-16 14:57:11 +0000

    test: fix single upload webpack tests due to change in webpack format

--- features/expected/single-source-map-webpack/minified-file.js
+++ features/expected/single-source-map-webpack/minified-file.js
@@ -1,2 +1,2 @@
-(()=>{var o={945:()=>{console.log("hi from a")},262:()=>{console.log("hi from b")}},r={};function e(n){if(r[n])return r[n].exports;var s=r[n]={exports:{}};return o[n](s,s.exports,e),s.exports}console.log("hi"),e(945),e(262)})();
+(()=>{var o={945:()=>{console.log("hi from a")},262:()=>{console.log("hi from b")}},r={};function e(n){var s=r[n];if(void 0!==s)return s.exports;var t=r[n]={exports:{}};return o[n](t,t.exports,e),t.exports}console.log("hi"),e(945),e(262)})();
 //# sourceMappingURL=main.js.map
